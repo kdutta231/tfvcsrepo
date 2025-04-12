32ec4cb616e12cf6c67f3664292041327a591bda
kdutta231 <duttakunal231@gmail.com>
2025-04-12 13:03:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 terraform {
-  cloud {
-    organization = "duttakunal231-org"
-
-    workspaces {
-      name = "kduttalab"
-    }
-  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
